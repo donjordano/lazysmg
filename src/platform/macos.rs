63742a1a7564c89c15998d719a8cb691c132084a
donjordano <jordanov.ivan@gmail.com>
2025-04-14 23:27:13 +0300

    code formating

--- src/platform/macos.rs
+++ src/platform/macos.rs
@@ -1,5 +1,5 @@
-use sysinfo::{DiskExt, System, SystemExt};
 use std::process::Command;
+use sysinfo::{DiskExt, System, SystemExt};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct StorageDevice {
@@ -21,64 +21,64 @@ pub fn detect_storage_devices() -> Vec<StorageDevice> {
     sys.refresh_disks_list();
     sys.refresh_disks();
 
-    sys.disks().iter().map(|disk| {
-        let mount_str = disk.mount_point().to_string_lossy().to_string();
-        // Consider device ejectable if mount point starts with "/Volumes/"
-        let ejectable = mount_str.starts_with("/Volumes/");
+    sys.disks()
+        .iter()
+        .map(|disk| {
+            let mount_str = disk.mount_point().to_string_lossy().to_string();
+            // Consider device ejectable if mount point starts with "/Volumes/"
+            let ejectable = mount_str.starts_with("/Volumes/");
 
-        // Try to gather extra info using "diskutil info"
-        let vendor_info = {
-            let output = Command::new("diskutil")
-                .arg("info")
-                .arg(&mount_str)
-                .output();
+            // Try to gather extra info using "diskutil info"
+            let vendor_info = {
+                let output = Command::new("diskutil")
+                    .arg("info")
+                    .arg(&mount_str)
+                    .output();
 
-            if let Ok(output) = output {
-                let info_str = String::from_utf8_lossy(&output.stdout);
-                let mut media = None;
-                let mut protocol = None;
-                let mut fs_type = None;
-                for line in info_str.lines() {
-                    if line.contains("Device / Media Name:") {
-                        media = line.split(':').nth(1)
-                                    .map(|s| s.trim().to_string());
-                    } else if line.contains("Protocol:") {
-                        protocol = line.split(':').nth(1)
-                                       .map(|s| s.trim().to_string());
-                    } else if line.contains("File System Personality:") {
-                        fs_type = line.split(':').nth(1)
-                                     .map(|s| s.trim().to_string());
+                if let Ok(output) = output {
+                    let info_str = String::from_utf8_lossy(&output.stdout);
+                    let mut media = None;
+                    let mut protocol = None;
+                    let mut fs_type = None;
+                    for line in info_str.lines() {
+                        if line.contains("Device / Media Name:") {
+                            media = line.split(':').nth(1).map(|s| s.trim().to_string());
+                        } else if line.contains("Protocol:") {
+                            protocol = line.split(':').nth(1).map(|s| s.trim().to_string());
+                        } else if line.contains("File System Personality:") {
+                            fs_type = line.split(':').nth(1).map(|s| s.trim().to_string());
+                        }
                     }
-                }
-                let mut info_vec = Vec::new();
-                if let Some(fs) = fs_type {
-                    info_vec.push(format!("FS: {}", fs));
-                }
-                if let Some(manu) = media {
-                    info_vec.push(format!("Manufacturer: {}", manu));
-                }
-                if let Some(proto) = protocol {
-                    info_vec.push(format!("Protocol: {}", proto));
-                }
-                if !info_vec.is_empty() {
-                    Some(info_vec.join(", "))
+                    let mut info_vec = Vec::new();
+                    if let Some(fs) = fs_type {
+                        info_vec.push(format!("FS: {}", fs));
+                    }
+                    if let Some(manu) = media {
+                        info_vec.push(format!("Manufacturer: {}", manu));
+                    }
+                    if let Some(proto) = protocol {
+                        info_vec.push(format!("Protocol: {}", proto));
+                    }
+                    if !info_vec.is_empty() {
+                        Some(info_vec.join(", "))
+                    } else {
+                        None
+                    }
                 } else {
                     None
                 }
-            } else {
-                None
-            }
-        };
+            };
 
-        StorageDevice {
-            name: disk.name().to_string_lossy().to_string(),
-            total_space: disk.total_space(),
-            available_space: disk.available_space(),
-            mount_point: mount_str,
-            ejectable,
-            vendor_info,
-        }
-    }).collect()
+            StorageDevice {
+                name: disk.name().to_string_lossy().to_string(),
+                total_space: disk.total_space(),
+                available_space: disk.available_space(),
+                mount_point: mount_str,
+                ejectable,
+                vendor_info,
+            }
+        })
+        .collect()
 }
 
 /// Ejects a storage device on macOS by invoking "diskutil eject <mount_point>".
@@ -95,6 +95,7 @@ pub fn eject_device(device: &StorageDevice) -> Result<(), Box<dyn std::error::Er
         Err(format!(
             "diskutil error: {}",
             String::from_utf8_lossy(&output.stderr)
-        ).into())
+        )
+        .into())
     }
 }
